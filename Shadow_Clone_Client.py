c0b7be6176a8b11168bc401357326377a72b80ae
alhaponyfaraj <alhaponyfarag@gmail.com>
2020-10-25 11:08:41 +0200

    Adding more prompt options for program life cycle

--- Shadow_Clone_Client.py
+++ Shadow_Clone_Client.py
@@ -13,15 +13,25 @@ print(open("style.txt", "r").read())
 # content is the string to send, choice is the option to be send to the server
 def shadow_clone(content, choice):
     ''''''
-    client_socket.connect((host, port))  # connect to the server
+    data = ''
+    try:
+        client_socket.connect((host, port))  # connect to the server
 
-    choice_message = choice
+        choice_message = choice
 
-    client_socket.send(choice_message.encode())  # send choice message
-    client_socket.send(content.encode())  # send choice message
-    data = client_socket.recv(1024).decode()  # receive response
-    print('Received from server: ' + str(data))  # show in terminal
-    client_socket.close()  # close the connection
+        client_socket.send(choice_message.encode())  # send choice message
+        client_socket.send(content.encode())  # send choice message
+        data = client_socket.recv(1024).decode()  # receive response
+        print('Received from server: ' + str(data))  # show in terminal
+        client_socket.close()  # close the connection
+    except Exception as e:
+        Error_msg = '''There were an error ! Please Check if you can reach the server
+        Check the following for more details: \n'''
+        print(Error_msg + str(e))
+        input('''Press Enter to continue ...
+        if you want exit type 1 and press enter''')
+
+
     return data
 
 
@@ -44,17 +54,23 @@ def main():
         if choice == "1":
             string_to_send = shadow_clone(input("Type the string to send: "), choice)
 
-            print(" this is choice 1")
+            print('''this is choice 1
+            
+            ################################################################''')
         elif choice == "2":
             # send the value of a and b
             a = input("Type the value of a: ")
             b = input("Type the value of b:")
 
             string_to_send = shadow_clone(a + ',' + b, choice)
-            print(" this is choice 2")
+            print('''this is choice 2
+            
+            ################################################################''')
             print(string_to_send)
         elif choice == "3":
-            print(" this is exit choice")
+            print('''this is choice 3 Exiting !!!
+            
+            ################################################################''')
             client_socket.close()  # close the connection
             run_loop = False
         else:
