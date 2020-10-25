603e33e80c852655f0952182e7ee61d62c5f4c58
alhaponyfaraj <alhaponyfarag@gmail.com>
2020-10-25 10:38:46 +0200

    isolate the socket connection

--- Shadow_Clone_Client.py
+++ Shadow_Clone_Client.py
@@ -4,7 +4,6 @@ host = '127.0.0.1'  # as both code is running on same pc
 port = 50249  # socket server port number
 
 client_socket = socket()  # instantiate
-client_socket.connect((host, port))  # connect to the server
 
 # txt file contains the cli interface components
 #cli_style = open("style.txt", "r")
@@ -14,23 +13,32 @@ print(open("style.txt", "r").read())
 # content is the string to send, choice is the option to be send to the server
 def shadow_clone(content, choice):
     ''''''
+    client_socket.connect((host, port))  # connect to the server
+
     choice_message = choice
 
     client_socket.send(choice_message.encode())  # send choice message
     client_socket.send(content.encode())  # send choice message
     data = client_socket.recv(1024).decode()  # receive response
     print('Received from server: ' + str(data))  # show in terminal
-
+    client_socket.close()  # close the connection
     return data
 
 
 def main():
     run_loop = True
     while run_loop:
-        choice = input('''Type 1 or 2 to run a function: ---
+        choice = input('''Type 1 or 2 to run a function:-
+        
         1. Function A . 
+        _____________________________________________________
+        
         2. Function B .
+        _____________________________________________________
+        
         3. Exit .
+        _____________________________________________________
+        
          Your Choice: ''')
 
         if choice == "1":
@@ -38,10 +46,11 @@ def main():
 
             print(" this is choice 1")
         elif choice == "2":
+            # send the value of a and b
             a = input("Type the value of a: ")
             b = input("Type the value of b:")
-            print(a +','+ b)
-            string_to_send = shadow_clone(a +',' + b, choice)
+
+            string_to_send = shadow_clone(a + ',' + b, choice)
             print(" this is choice 2")
             print(string_to_send)
         elif choice == "3":
